004481a7feb1d99ee4caf485f70c5adb8f398090
nezaodooco <bruce_gabriel_rossini@hotmail.com>
2020-04-08 03:10:11 -0300

    Correção do erro - Pedido estava sem AUTOINCRIMENT

--- src/main/java/com/bruce/cursomc/domain/Pedido.java
+++ src/main/java/com/bruce/cursomc/domain/Pedido.java
@@ -5,6 +5,8 @@ import java.util.Date;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
@@ -15,6 +17,7 @@ public class Pedido implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private Date instante;
 	
